b799fd13032fe0266ce22fbc9cc708ff59bad060
colin-lallauret <141689095+kolinnnn@users.noreply.github.com>
2024-05-13 17:47:02 +0200

    Save

---
+++ spaceInvader/src/index.js
@@ -0,0 +1,9 @@
+import Space from '../assets/images/space.png';
+
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+canvas.height=600;
+canvas.width=600;
+
+const background = new Image();
+background.src = Space;
